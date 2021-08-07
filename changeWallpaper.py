d3b7e4641bd531aebc25d9ce5d624139d46163f1
Shameemudheen M P <shameemmanalaya26@gmail.com>
2021-08-07 14:12:55 +0530

    First commit

---
+++ changeWallpaper.py
@@ -0,0 +1,44 @@
+'''
+1. Download a favorite wallpaper from unsplash
+2. Save to a temp directory
+3. Set wallpaper
+
+'''
+
+import requests
+import urllib.request
+import ctypes
+
+def get_wallpaper():
+    #print("Hello world")
+    access_key = 'ybDMfAn0RQSfNXPF-PhmX9-Ph17d7kgcwsnGjgXkMAk'
+    url = 'https://api.unsplash.com/photos/random?client_id='+access_key
+    params = {
+        #these are the parameters of random image api
+        "query" : "HD Wallpapers",
+        "orientation" : "landscape"
+    }
+
+    response = requests.get(url,params).json() #generating response object , ie , it is a json object
+    image_url = response['urls']['full'] #finding image url from response 
+    wallpaper = urllib.request.urlretrieve(image_url, '/Users/HP/Pictures/Wallpaper Python/wallpaper.jpg') #downloading image from image_url to the specified directory and with specified name
+    print(wallpaper)
+    print("Wallpaper succefully downloaded...")
+
+
+def set_wallpaper():
+    get_wallpaper()
+    #python command to set wallpaper in windows
+    ctypes.windll.user32.SystemParametersInfoA(20, 0, 'c:/Users/HP/Pictures/Wallpaper Python/wallpaper.jpg'.encode() , 0)
+    print("Wallpaper succefully changed...")
+
+def main():
+    op = "n"
+    while op=="n":
+        set_wallpaper()
+        op = input("Confirm the current wallpaper ? (y/n)")
+
+
+if __name__ == '__main__':
+    main()
+
